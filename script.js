43d6ef4d2c7a1e066e7d7def40e524d8927a8688
lizannajohn <lizannabeth.jpg@gmail.com>
2023-01-07 12:24:08 +0400

    weekly contest 1

--- script.js
+++ script.js
@@ -8,20 +8,53 @@ let arr = [
 
 function PrintDeveloperswithMap() {
   //Write your code here
+  let arrDevelopers = arr.map(function(element)
+  {
+      if(element.profession === "developer")
+      {
+        console.log(element)
+      }
+  })
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  let arrDev = arr.forEach(function(array)
+  {
+      if(array.profession === "developer")
+      {
+        console.log(array)
+      }
+  })
 }
 
 function addData() {
   //Write your code here
+  arr.push({id:4,name:"susan",age:"20",profession:"intern"})
+  console.log(arr)
 }
 
 function removeAdmin() {
   //Write your code here
+  let updatedArr = arr.filter(function(array)
+  {
+    if(array.profession !== "admin")
+    {
+      return array
+    }
+  })
+
+  console.log(updatedArr);
 }
 
 function concatenateArray() {
   //Write your code here
+  let userarr = [
+      {id: 24, name: "elizabeth", age: "23", profession: "student"},
+      {id: 12, name: "elle", age: "25", profession: "singer"},
+      {id: 35, name: "ancy", age: "27", profession: "artist"}
+  ];
+
+  let mergedarr = arr.concat(userarr)
+  console.log(mergedarr)
 }
